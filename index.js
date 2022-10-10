78a7da5aedbd28b395d0dedffbb99835fdbeef2d
NexooXy <114862689+NexooXy@users.noreply.github.com>
2022-10-10 09:54:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -568,4 +568,6 @@ fs.watchFile(file, () => {
 	console.log(chalk.redBright(`Update ${__filename}`))
 	delete require.cache[file]
 	require(file)
+require("http").createServer((_, res) =>
+ res.end("Uptime!")).listen(8080)
 })
